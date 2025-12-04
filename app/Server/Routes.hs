941437a9ba0a6139f32723dd5749500c224bdc90
Giovanna Pennisi <83779918+giovannapennisi@users.noreply.github.com>
2025-12-03 21:23:48 -0300

    adicionando as opçoes de editar e excluir

--- app/Server/Routes.hs
+++ app/Server/Routes.hs
@@ -2,59 +2,98 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Server.Routes where 
+
+module Server.Routes where
 
 import Api.Model
-import Data.Proxy
-import Network.Wai
-import Servant.API.Sub
-import Servant.API
-import Servant.Server
-import Database.PostgreSQL.Simple
 import Control.Monad.IO.Class
 import Control.Monad.Except
+import Database.PostgreSQL.Simple
+import Data.Proxy
+import Data.Aeson (Value, object, (.=))
+import Network.Wai
+import Servant.API
+import Servant.Server
 
-type API = 
-         "aluno" :> ReqBody '[JSON] Aluno :> Post '[JSON] ResultadoResponse 
-    :<|> "aluno"  :> Verb 'OPTIONS 200 '[JSON] ()
+type API =
+         "aluno" :> ReqBody '[JSON] Aluno :> Post '[JSON] ResultadoResponse
+    :<|> "aluno" :> Capture "id" Int :> Get '[JSON] Value
+    :<|> "aluno" :> Capture "id" Int :> ReqBody '[JSON] Aluno :> Put '[JSON] ResultadoResponse
+    :<|> "aluno" :> Capture "id" Int :> DeleteNoContent
+    :<|> "aluno" :> Capture "id" Int :> Verb 'OPTIONS 200 '[JSON] ()
+    :<|> "aluno" :> Verb 'OPTIONS 200 '[JSON] ()
     :<|> "aluno" :> Get '[JSON] AlunoResponse
 
-handlerAlunoTodos :: Connection -> Handler AlunoResponse
-handlerAlunoTodos conn = do 
-    res <- liftIO $ query_ conn 
-        "SELECT id, nome, cpf, telefone, idade, peso, altura FROM Aluno" 
-    let result = map 
-            (\(id', nome', cpf', telefone', idade', peso', altura') ->
-                Aluno id' nome' cpf' telefone' idade' peso' altura') 
-            res
-    pure (AlunoResponse result)
 
 handlerAluno :: Connection -> Aluno -> Handler ResultadoResponse
-handlerAluno conn alu = do 
+handlerAluno conn alu = do
     res <- liftIO $
         query conn
             "INSERT INTO Aluno (nome, cpf, telefone, idade, peso, altura) \
             \VALUES (?,?,?,?,?,?) RETURNING id"
-            ( nome alu
-            , cpf alu
-            , telefone alu
-            , idade alu
-            , peso alu
-            , altura alu
-            )
-    case res of 
+            (nome alu, cpf alu, telefone alu, idade alu, peso alu, altura alu)
+
+    case res of
         [Only novoId] -> pure (ResultadoResponse novoId)
         _ -> throwError err500
+
+handlerAlunoTodos :: Connection -> Handler AlunoResponse
+handlerAlunoTodos conn = do 
+    res <- liftIO $
+        query_ conn "SELECT id, nome, cpf, telefone, idade, peso, altura FROM Aluno"
+
+    let result = map
+            (\(id', n, c, t, i, p, a) -> Aluno id' n c t i p a)
+            res
+
+    pure (AlunoResponse result)
+
+handlerAlunoPorId :: Connection -> Int -> Handler Value
+handlerAlunoPorId conn ident = do
+    res <- liftIO $
+        query conn
+            "SELECT id, nome, cpf, telefone, idade, peso, altura FROM Aluno WHERE id = ?"
+            (Only ident)
+
+    case res of
+        [(id', n, c, t, i, p, a)] ->
+            pure $ object
+                [ "aluno" .= Aluno id' n c t i p a ]
+        _ -> throwError err404
+        
+handlerAtualizar :: Connection -> Int -> Aluno -> Handler ResultadoResponse
+handlerAtualizar conn ident alu = do
+    _ <- liftIO $
+        execute conn
+            "UPDATE Aluno SET nome=?, cpf=?, telefone=?, idade=?, peso=?, altura=? WHERE id=?"
+            (nome alu, cpf alu, telefone alu, idade alu, peso alu, altura alu, ident)
+    pure (ResultadoResponse ident)
+
+
+handlerExcluir :: Connection -> Int -> Handler NoContent
+handlerExcluir conn ident = do
+    _ <- liftIO $
+        execute conn "DELETE FROM Aluno WHERE id=?" (Only ident)
+    pure NoContent
 
 
 options :: Handler ()
 options = pure ()
 
+optionsId :: Connection -> Int -> Handler ()
+optionsId _ _ = pure ()
 
-server :: Connection -> Server API 
-server conn =    handlerAluno conn 
-            :<|> options 
-            :<|> handlerAlunoTodos conn
+
+server :: Connection -> Server API
+server conn =
+       handlerAluno conn
+    :<|> handlerAlunoPorId conn
+    :<|> handlerAtualizar conn
+    :<|> handlerExcluir conn
+    :<|> optionsId conn 
+    :<|> options
+    :<|> handlerAlunoTodos conn
+
 
 addCorsHeader :: Middleware
 addCorsHeader app' req resp =
@@ -62,11 +101,11 @@ addCorsHeader app' req resp =
     resp $ mapResponseHeaders
       ( \hs ->
           [ ("Access-Control-Allow-Origin", "*")
-          , ("Access-Control-Allow-Headers", "Content-Type, Authorization")
-          , ("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+          , ("Access-Control-Allow-Headers", "content-type")
+          , ("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
           ] ++ hs
       )
       res
 
-app :: Connection -> Application 
+app :: Connection -> Application
 app conn = addCorsHeader (serve (Proxy @API) (server conn))
--- frontend/aluno.js
+++ frontend/aluno.js
@@ -1,15 +1,23 @@
-function servidor(){
+let editandoId = null;
+
+function servidor() {
     let AlunoNome = document.querySelector("#nome").value;
     let AlunoCPF = document.querySelector("#cpf").value;
     let AlunoTelefone = document.querySelector("#telefone").value;
     let AlunoIdade = parseInt(document.querySelector("#idade").value);
     let AlunoPeso = parseFloat(document.querySelector("#peso").value);
     let AlunoAltura = parseFloat(document.querySelector("#altura").value);
-    fetch("http://localhost:8080/aluno",{
-        method: "POST",
-        headers: {"content-type" : "application/json"},
+
+    let metodo = editandoId ? "PUT" : "POST";
+    let url = editandoId 
+        ? `http://localhost:8080/aluno/${editandoId}` 
+        : "http://localhost:8080/aluno";
+
+    fetch(url, {
+        method: metodo,
+        headers: { "content-type": "application/json" },
         body: JSON.stringify({
-            id: 0,
+            id: editandoId ?? 0,
             nome: AlunoNome,
             cpf: AlunoCPF,
             telefone: AlunoTelefone,
@@ -20,15 +28,88 @@ function servidor(){
     })
     .then(response => response.json())
     .then(json => {
-        document.querySelector("#res").innerHTML = "id: " + json.resultado
+        document.querySelector("#res").innerHTML = editandoId
+            ? "Aluno atualizado!"
+            : "Aluno cadastrado! ID: " + json.resultado;
+
+        editandoId = null;
+        document.querySelector("#btn").innerText = "Cadastrar";
+
+        atualizarAlunos();
     })
-    .catch(error => alert(error))
+    .catch(error => alert(error));
 }
 
 function teste(){
-    document.querySelector("#btn").addEventListener("click", () => {
-        servidor()
-    })
+    document.querySelector("#btn").addEventListener("click", servidor);
 }
 
-window.onload = teste
+function atualizarAlunos() {
+    fetch("http://localhost:8080/aluno")
+        .then(response => response.json())
+        .then(json => {
+            let tabela = document.querySelector("#tabelaAlunos tbody");
+            tabela.innerHTML = "";
+
+            json.alunos.forEach(alu => {
+                let linha = `
+                    <tr>
+                        <td>
+                            <button onclick="editarAluno(${alu.id})">Editar</button>
+                            <button onclick="excluirAluno(${alu.id})">Excluir</button>
+                        </td>
+                        <td>${alu.id}</td>
+                        <td>${alu.nome}</td>
+                        <td>${alu.cpf}</td>
+                        <td>${alu.telefone}</td>
+                        <td>${alu.idade}</td>
+                        <td>${alu.peso}</td>
+                        <td>${alu.altura}</td>
+                    </tr>
+                `;
+                tabela.innerHTML += linha;
+            });
+        })
+        .catch(err => alert("Erro ao atualizar alunos: " + err));
+}
+
+function excluirAluno(id) {
+    if (!confirm("Tem certeza que deseja excluir o aluno " + id + "?")) return;
+
+    fetch(`http://localhost:8080/aluno/${id}`, {
+        method: "DELETE"
+    })
+    .then(res => {
+        if (res.ok) {
+            alert("Aluno excluído!");
+            atualizarAlunos();
+        } else {
+            alert("Erro ao excluir aluno.");
+        }
+    })
+    .catch(err => alert("Erro: " + err));
+}
+
+function editarAluno(id) {
+    fetch(`http://localhost:8080/aluno/${id}`)
+        .then(res => res.json())
+        .then(json => {
+            let alu = json.aluno;
+
+            document.querySelector("#nome").value = alu.nome;
+            document.querySelector("#cpf").value = alu.cpf;
+            document.querySelector("#telefone").value = alu.telefone;
+            document.querySelector("#idade").value = alu.idade;
+            document.querySelector("#peso").value = alu.peso;
+            document.querySelector("#altura").value = alu.altura;
+
+            editandoId = id;
+
+            document.querySelector("#btn").innerText = "Salvar Edição";
+        });
+}
+
+window.onload = () => {
+    teste();
+    atualizarAlunos();
+};
